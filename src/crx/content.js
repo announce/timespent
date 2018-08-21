32f633760d148807be9596f1ec52166397b71d6b
Kenta Yamamoto <lq33xxx@gmail.com>
2018-08-22 03:13:44 +0900

    Added language switcher

--- src/crx/content.js
+++ src/crx/content.js
@@ -1,58 +1,26 @@
 // @flow
 'use strict'
+const fs = require('fs')
+const path = require('path')
 const $ = require('jquery')
+const ejs = require('ejs')
 const satisfactionImages = require('./image/base64')
 
 const createLanguageSwitcherElement = () => {
-  return `
-    <form style="padding:2em 0;">
-       <h4>Switch the language</h4>
-       <select style="font-size:2em;border:1em;">
-        <option value="en-us">English</option>
-        <option value="ja-jp">Japanese</option>
-    </select>
-    </form>
-  `
+  return ejs.render(fs.readFileSync(path.resolve('src/crx/language-switcher.html.ejs'), 'utf-8'), {})
 }
 
 const createSatisfactionElement = () => {
-  const smilesElement = satisfactionImages.map((dict: Object) => {
-    return `<img
-    width="40em;" src="${dict.mono}" data-src="${dict.color}"
-    onmouseover="cacSwapImage(this)"
-    onmouseout="cacSwapImage(this)"
-  >`
+  return ejs.render(fs.readFileSync(path.resolve('src/crx/satisfaction-feedback.html.ejs'), 'utf-8'), {
+    satisfactionImages: satisfactionImages
   })
-
-  return `
-    <script>
-    window.cacSwapImage = (target) => {
-      let $t = $(target)
-      let s1 = $t.attr('data-src')
-      let s2 = $t.attr('src')
-      $t.attr('data-src', s2)
-      $t.attr('src', s1)
-    }
-    </script>
-    <div class="ask-a-question-panel">
-        <h4>This page is translated from English</h4>
-        <div>${smilesElement.join('')}</div>
-        <p>Let us know your satisfaction on translation!</p>
-    </div>
-  `
 }
 
 /**
  * https://developer.chrome.com/extensions/content_scripts
- * @param w
- * @param chrome
  */
-const main = (w: Object, chrome: Object) => {
+$(() => {
   $('.page-sidebars')
     .prepend(createLanguageSwitcherElement())
     .append(createSatisfactionElement())
-}
-
-$(() => {
-  main(window, chrome)
 })
