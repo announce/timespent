fff853d710e6756f45304a4a2f98703988c9ec3e
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2018-08-22 12:12:44 +0900

    Added href to satisfaction feedback images

--- src/crx/content.js
+++ src/crx/content.js
@@ -4,15 +4,27 @@ const fs = require('fs')
 const path = require('path')
 const $ = require('jquery')
 const ejs = require('ejs')
-const satisfactionImages = require('./image/base64')
+const satisfaction = require('./image/base64')
+const app = require('./app')
 
 const createLanguageSwitcherElement = () => {
   return ejs.render(fs.readFileSync(path.resolve('src/crx/language-switcher.html.ejs'), 'utf-8'), {})
 }
 
 const createSatisfactionElement = () => {
+  const url = new URL(chrome.extension.getURL('form.html'))
+  url.searchParams.set('url', encodeURIComponent(window.location.href))
+  const sat = satisfaction.map((value, index) => {
+    url.searchParams.set('score', 5 - index)
+    return {...{
+      href: url.href
+    },
+    ...value}
+  })
+  console.log('satisfaction:', sat)
   return ejs.render(fs.readFileSync(path.resolve('src/crx/satisfaction-feedback.html.ejs'), 'utf-8'), {
-    satisfactionImages: satisfactionImages
+    app: app,
+    satisfaction: sat
   })
 }
 
@@ -23,8 +35,4 @@ $(() => {
   $('.page-sidebars')
     .prepend(createLanguageSwitcherElement())
     .append(createSatisfactionElement())
-  // @TODO
-  const url = new URL(chrome.extension.getURL('form.html'))
-  url.searchParams.set('aaa', 1)
-  window.open(url, 'SingleSecondaryWindowName', 'resizable,scrollbars')
 })
